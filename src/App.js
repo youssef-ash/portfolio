f3a418a943229d39c3c57274d94f2df6cef6dd85
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-19 14:35:02 +0200

    added dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import Nav from './components/nav';
 import Footer from './components/footer';
 import Home from './components/home';
+import About from './components/about';
+import { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+
+  /* State Declarations */
+  const [firstLoaded, setFirstLoaded] = useState(false);
+  const [dark, setDark] = useState(false);
+  const [viewing, setViewing] = useState('Home');
+
+  /* Effects */
+
+  // loads component in dark mode if the last set theme was that
+  useEffect(() => {
+    const prefersDark = localStorage.getItem('darkMode');
+    if (prefersDark === 'enabled') {
+      setDark(true);
+    }
+  }, []);
+
+  // applies transitions after start component is loaded to prevent light mode styling flashing before dark mode styling
+  useEffect(() => {
+    setTimeout(() => {
+      setFirstLoaded(true)
+    }, 100);
+  }, []);
+
+  /* Main Functions */
+
+  // toggles dark mode
+  function toggleDarkMode() {
+    if (dark) {
+      setDark(false);
+      localStorage.setItem('darkMode', 'disabled');
+    } else {
+      setDark(true);
+      localStorage.setItem('darkMode', 'enabled');
+    }
+  };
+
   return (
-    <div className='app'>
-      <Nav />
-      <Home />
+    <div className={`app ${dark ? 'dark-mode' : ''} ${firstLoaded ? 'loaded' : ''}`}>
+      <Nav darkMode={dark} toggleDarkMode={toggleDarkMode} setViewing={setViewing} />
+      {viewing === 'Home' && <Home />}
+      {viewing === 'About' && <About />}
+      {/* {viewing === 'Contact' && <Contact />} */}
       <Footer />
     </div>
   );
 }
-
-export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,16 @@
-export default function Nav() {
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+
+export default function Nav({darkMode, toggleDarkMode, setViewing}) {
   return (
     <nav className='nav'>
-      <button className='nav__dark-mode'></button>
+      <button className='nav__dark-mode' title={darkMode ? 'Switch to light mode' : 'Switch to dark mode'} onClick={toggleDarkMode}>
+        {darkMode ? <MdLightMode /> : <MdDarkMode />}
+      </button>
       <div className='nav__list'>
-        <a href='' className='nav__link'>About</a>
-        <a href='' className='nav__link'>Projects</a>
-        <a href='' className='nav__link'>Contact</a>
+        <a className='nav__link' onClick={() => {setViewing('Home')}}>Home</a>
+        <a className='nav__link' onClick={() => {setViewing('About')}}>About</a>
+        <a className='nav__link' onClick={() => {setViewing('Projects')}}>Projects</a>
+        <a className='nav__link' onClick={() => {setViewing('Contact')}}>Contact</a>
       </div>
     </nav>
   )

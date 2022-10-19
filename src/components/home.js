6f129c2dd36c8d42f7700472143af2139b4368ae
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-19 13:55:06 +0200

    updated home component class names

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,14 @@
 export default function Home() {
   return (
-    <main className='main'>
-      <div className='main__header'>
-        <h2 className='main__greeting'>Hi There!</h2>
-        <h1 className='main__introduction'>
-          I'm <span>Youssef Ahmed</span> <br /> An Aspiring Web Developer.
+    <main className='home'>
+      <div className='home__header'>
+        <h2 className='home__greeting'>Hi There!</h2>
+        <h1 className='home__introduction'>
+          I'm <span>Youssef Ahmed</span> <br />
+          An Aspiring Web Developer.
         </h1>
       </div>
-      <img className='main__image' src='images/web_dev_image.png' alt='programmer vector image' />
+      <img className='home__image' src='images/web_dev_image.png' alt='programmer vector' />
     </main>
   )
 }

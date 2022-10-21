5f52a0b5c89fa40a79085a50e8a3d5ca514955d2
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-21 14:31:25 +0200

    added projects data for projects page

---
+++ src/components/config/projectsData.js
@@ -0,0 +1,32 @@
+import triviaAppImg from './projectImages/trivia_app.png';
+import weatherAppImg from './projectImages/weather_journal_app.png';
+import portfolioImg from './projectImages/portfolio_light.png';
+
+export const projectsData = [
+  {
+    id: 1,
+    title: 'Trivia App',
+    image: triviaAppImg,
+    description:
+      'A single page React application that utilizes the Open Trivia Database API to generate trivia questions based on the options the user chooses.',
+    githubLink: 'https://github.com/youssef-ash/trivia-app',
+    liveLink: '',
+  },
+  {
+    id: 2,
+    title: 'Weather Journal App',
+    image: weatherAppImg,
+    description:
+      'A simple JavaScript application that utilizes the OpenWeather API to fetch the current weather in the USA zip code entered by the user.',
+    githubLink: 'https://github.com/youssef-ash/weather-journal',
+    liveLink: '',
+  },
+  {
+    id: 3,
+    title: 'Portfolio',
+    image: portfolioImg,
+    description: '',
+    githubLink: 'https://github.com/youssef-ash/portfolio',
+    liveLink: '',
+  },
+];

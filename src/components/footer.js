9c7c620aa61f0e06ab0697867dcda44d1539881a
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-23 22:23:21 +0200

    formatted code

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,15 +3,28 @@ import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="mailto:youssefashwal@outlook.com" className="footer__link">
-        <FaEnvelope />
-      </a>
-      <a target={'_blank'} href="https://github.com/youssef-ash" rel="noreferrer" className="footer__link">
-        <FaGithub />
-      </a>
-      <a target={'_blank'} href="https://www.linkedin.com/in/youssef-ash/" rel="noreferrer" className="footer__link">
-        <FaLinkedin />
-      </a>
+      <div className="footer__copyright">Designed & Built by Youssef Ahmed</div>
+      <div className="footer__links__container">
+        <a href="mailto:youssefashwal@outlook.com" className="footer__link">
+          <FaEnvelope />
+        </a>
+        <a
+          target={'_blank'}
+          href="https://github.com/youssef-ash"
+          rel="noreferrer"
+          className="footer__link"
+        >
+          <FaGithub />
+        </a>
+        <a
+          target={'_blank'}
+          href="https://www.linkedin.com/in/youssef-ash/"
+          rel="noreferrer"
+          className="footer__link"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </footer>
   );
 }

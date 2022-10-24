d9e170afc797d7baf2b4682993b23ed1d2ec5496
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-24 12:45:06 +0200

    formatted code

--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <main className="home">
       <div className="home__header">
-        <h2 className="home__greeting">Hi There!</h2>
-        <h1 className="home__introduction">
+        <h1 className="home__greeting">Hi There!</h1>
+        <h2 className="home__introduction">
           I'm <span>Youssef Ahmed</span> <br />
           An Aspiring Web Developer.
-        </h1>
+        </h2>
       </div>
       <img
         className="home__image"

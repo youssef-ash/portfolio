b1c7fbce50f9bfce52ed0b2fcaca142fbef78014
Youssef Ahmed <youssefashwal@outlook.com>
2022-10-30 03:08:30 +0200

    finished contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,28 @@
 export default function Contact() {
   return (
     <main className="contact">
-      <h1>Contact</h1>
+      <h1>
+        Contact <span>Me</span>
+      </h1>
+      <p>
+        I'm currently looking for opportunities. <br />
+        If you're interested connect with me on{' '}
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/youssef-ash/"
+          rel="noreferrer"
+          className="link"
+        >
+          LinkedIn
+        </a>{' '}
+        or send me an e&#8209;mail using the button below.
+      </p>
+      <a
+        className="button contact__button"
+        href="mailto:youssefashwal@outlook.com"
+      >
+        E-mail me
+      </a>
     </main>
   );
 }
